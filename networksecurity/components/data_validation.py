bb4ee9f8eb8b5a50e93a21633711d5e61917b28f
shambhavi <sm1066279@gmail.com>
2025-02-03 20:27:08 +0530

    Data Validation completed

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -11,7 +11,7 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
-from networksecurity.utils.main_utils.utils import read_yaml_file
+from networksecurity.utils.main_utils.utils import read_yaml_file,write_yaml_file
 
 class DataValidation:
 
@@ -39,25 +39,45 @@ class DataValidation:
             raise NetworkSecurityException(e,sys)
         
 
+        #null hypothesis= no difference,
+        #A small p-value indicates strong evidence against the null hypothesis 
+        #A large p-value suggests weak evidence against the null hypothesis
+        #alternative hypo= there is an effect or a difference.
+
     def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
         try:
-            status=True
-            report={}
-            for column in base_df.columns:
+            status=True # Assume NO DRIFT initially
+            report={}   #This empty dictionary will store the p-values and drift status for each column.
+
+            for column in base_df.columns: # we are passing the same column name from base_df to current_df is because we are comparing the distribution of the same feature across both datasets.
                 d1=base_df[column]
                 d2=current_df[column]
-                is_same_dist=ks_2samp(d1,d2)
-                if threshold<=is_same_dist:
+                is_same_dist=ks_2samp(d1,d2) #ks_2samp() (Kolmogorov-Smirnov test) is used for comparing two distributions.
+                if threshold<=is_same_dist.pvalue:
                     is_found=False
                 else:
                     is_found=True
                     status=False
-                
-                report.update({column:})
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                    
+                    }})
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+
+            
+
+            #Create directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path,content=report)
+
 
         except Exception as e:
             raise NetworkSecurityException
     
+
+
 
     @staticmethod
     def read_data(file_path)->pd.DataFrame:
@@ -77,5 +97,33 @@ class DataValidation:
             train_dataframe=DataValidation.read_data(train_file_path)
             test_dataframe=DataValidation.read_data(test_file_path)
 
+            #Validate number of columns
+            status=self.validate_number_of_columns(dataframe=train_dataframe)
+            if not status:
+                error_message=f"Train dataframe does not contain all columns. \n"
+
+            status=self.validate_number_of_columns(dataframe=test_dataframe)
+            if not status:
+                error_message=f"Test dataframe does not contain all columns. \n"
+
+
+            #Data Drift
+            status=self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
+
+            dir_path=os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+
+            train_dataframe.to_csv(self.data_validation_config.valid_train_file_path,index=False,header=True)
+            test_dataframe.to_csv(self.data_validation_config.valid_test_file_path,index=False,header=True)
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+            return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -7,9 +7,24 @@ import numpy as np
 import dill
 import pickle
 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file: ##Opens the YAML file in binary read mode ("rb"), meaning it reads the file as bytes instead of a string.
             return yaml.safe_load(yaml_file)  #Parses it safely into a dictionary
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
+    
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
