f863b8c7c789547b159d90c7743321d713db71f9
shambhavi <sm1066279@gmail.com>
2025-02-10 17:09:19 +0530

    training evaluation and MLFLOWS

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,7 +25,7 @@ from sklearn.ensemble import (
 )
 
 
-
+import mlflow
 
 
 
@@ -36,6 +36,19 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            # Model registry does not work with file store
     
         
     def train_model(self,X_train,y_train,x_test,y_test):
@@ -89,6 +102,7 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+
         
         ## Track the experiements with mlflow
         self.track_mlflow(best_model,classification_train_metric)
